fe5b9eb2c780f76c1b3aeda7d3581350322a2bed
vaishnavi-satone <70249248+vaishnavi-satone@users.noreply.github.com>
2020-08-31 14:29:18 +0530

    File Added

---
+++ main.js
@@ -0,0 +1,7 @@
+
+
+function myFunction() {
+  document.getElementById("demo").style.fontSize = "25px";
+  document.getElementById("demo").style.color = "red";
+  document.getElementById("demo").style.backgroundColor = "yellow";
+}
